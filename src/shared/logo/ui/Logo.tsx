0cd1a963b39191fce196e3b84b11a74ea06ef71e
jKot59 <rcrew6694@gmail.com>
2026-04-23 10:17:31 +0300

    changed logo and favicon

--- src/shared/logo/ui/Logo.tsx
+++ src/shared/logo/ui/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <div className={styles.logo}>
       <div className={styles.image}>
-        <Image src='/workout-log-logo.png' alt='logo' width={38} height={43.8} priority />
+        <Image src='/workout-log-logo.png' alt='logo' fill priority />
       </div>
       <span>Workout log</span>
     </div>
