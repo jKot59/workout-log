89de18032ffc3be6944af9032b3d904bc4b9ee5d
jKot59 <rcrew6694@gmail.com>
2026-04-27 11:10:16 +0300

    logo leads to a home page

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import styles from './logo.module.scss';
+import Link from 'next/link';
+import { paths } from '@/shared/paths';
 
 export function Logo() {
   return (
-    <div className={styles.logo}>
+    <Link href={paths.home} className={styles.logo}>
       <div className={styles.image}>
         <Image src='/workout-log-logo.png' alt='logo' fill sizes='(max-width: 768px) 100% 100%' priority />
       </div>
       <span>Workout log</span>
-    </div>
+    </Link>
   );
 }
