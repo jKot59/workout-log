070cfdda4f83caf6ecdaf646419b6b527df31960
jKot59 <rcrew6694@gmail.com>
2026-05-06 11:41:39 +0300

    added exercises images

--- src/features/exercise/ui/AddExerciseDrawer.tsx
+++ src/features/exercise/ui/AddExerciseDrawer.tsx
@@ -4,15 +4,17 @@ import { Button } from '@/shared/ui/button/Button';
 import { DayOfWeek } from '@/widgets/appSider';
 import { Drawer } from 'antd';
 import { useAddExerciseDrawerLogic } from '../model/useAddExerciseDrawerLogic';
+import Image from 'next/image';
+import styles from './addExerciseDrawer.module.scss';
 
 const exercisesList = [
-  'Bulgarian split squats',
-  'Calf raises',
-  'Hindu push-ups',
-  'Parallel bar dips',
-  'Pull-ups',
-  'Knee to chest on parallel bars',
-  'Biceps pull-ups',
+  { name: 'Bulgarian split squats', imageURL: '/images/exercises/bulgarian_split_squats.jpeg' },
+  { name: 'Calf raises', imageURL: '/images/exercises/calf_raises.jpeg' },
+  { name: 'Hindu push-ups', imageURL: '/images/exercises/hindu_pushups.jpeg' },
+  { name: 'Parallel bar dips', imageURL: '/images/exercises/dips.jpeg' },
+  { name: 'Pull-ups', imageURL: '/images/exercises/pull_ups.jpeg' },
+  { name: 'Knee to chest on parallel bars', imageURL: '/images/exercises/knees_to_chest.jpeg' },
+  { name: 'Biceps pull-ups', imageURL: '/images/exercises/biceps_pull_ups.jpeg' },
 ];
 
 export function AddExerciseDrawer({ day }: { day: DayOfWeek }) {
@@ -22,12 +24,19 @@ export function AddExerciseDrawer({ day }: { day: DayOfWeek }) {
     <>
       <Button onClick={handlers.showDrawer}>Add exercise</Button>
 
-      <Drawer title='Basic Drawer' placement='bottom' closable={false} onClose={handlers.onClose} open={state.open}>
-        {exercisesList.map((exercise) => (
-          <div key={exercise} onClick={() => handlers.addNewExercise(exercise)}>
-            {exercise}
-          </div>
-        ))}
+      <Drawer title='Select the exercise' placement='bottom' closable={false} onClose={handlers.onClose} open={state.open}>
+        <div className={styles.drawer_content}>
+          {exercisesList.map((exercise) => (
+            <div key={exercise.name} className={styles.exercise} onClick={() => handlers.addNewExercise(exercise.name)}>
+              {exercise.name}
+              {exercise.imageURL && (
+                <div className={styles.image_wrapper}>
+                  <Image className={styles.image} src={exercise.imageURL} alt={exercise.name} fill sizes='200px 200px' loading='lazy' />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </Drawer>
     </>
   );
